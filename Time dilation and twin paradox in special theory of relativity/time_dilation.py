3c0ccebf2280b5ecfaccb1e83a8d61bb94f836af
Kiril Lazarov <kirillazarov@abv.bg>
2022-04-11 17:56:19 +0300

    Change formula in row 16

--- Time dilation and twin paradox in special theory of relativity/time_dilation.py
+++ Time dilation and twin paradox in special theory of relativity/time_dilation.py
@@ -3,27 +3,31 @@ from math import sqrt as sq
 
 def calculate_times_for_observer(sequence_spaceship_points_of_signals, data_dict, time_spaceship):
     y_coordinate = float(f'{data_dict["Distance_y"]:.2f}')
+    velocity = data_dict["Velocity"]
     sequence_observer = [0]
     time_receiving_signal = 0
+    print(f"Spaceship's time run {1 / time_spaceship:.2f} times slower than observer's time.")
+    start_point_x_axis = sequence_spaceship_points_of_signals[0]
+    print(f'Signal 1: 0 seconds  x coordinate of current signal {sequence_spaceship_points_of_signals[0]:.2f}')
     sequence_spaceship_points_of_signals = sequence_spaceship_points_of_signals[1:]
-    print(f'{1 / time_spaceship:.2f}')
-    print(f'Signal 1: 0 seconds')
     for i in range(len(sequence_spaceship_points_of_signals)):
         x_coordinate = sequence_spaceship_points_of_signals[i]
-        time_receiving_signal += 1 / time_spaceship + sq(y_coordinate ** 2 + x_coordinate ** 2)
+        curr_dist_travel_along_x_axis = abs(start_point_x_axis - x_coordinate)
+        time_receiving_signal =  curr_dist_travel_along_x_axis / velocity +  sq(y_coordinate ** 2 + x_coordinate ** 2)
         sequence_observer.append(time_receiving_signal)
         diff = sequence_observer[i + 1] - sequence_observer[i]
         print(f'Signal {i + 2}: {time_receiving_signal:.2f} seconds  '
-              f'Time interval between current and previous signal: {diff:.2f}')
+              f' x coordinate of current signal {sequence_spaceship_points_of_signals[i]} Time interval between current'
+              f' and previous signal: {diff:.2f}')
 
 
 def coordinates_spaceship_signals_points(data_dict, time_spaceship):
     x_coordinate = float(f'{data_dict["Distance_x"]:.2f}')
     velocity = float(data_dict["Velocity"])
     sequence_spaceship_points_of_signals = [-x_coordinate]
-    distance_per_time_spaceship = velocity / time_spaceship
+    position_change_spaceship_by_observer_time = 1 / time_spaceship * velocity
     for i in range(data_dict["Count_signals"]):
-        new_point = distance_per_time_spaceship + sequence_spaceship_points_of_signals[-1]
+        new_point = position_change_spaceship_by_observer_time + sequence_spaceship_points_of_signals[-1]
         sequence_spaceship_points_of_signals.append(float(f'{new_point:.2f}'))
     calculate_times_for_observer(sequence_spaceship_points_of_signals, data_dict, time_spaceship)
 
