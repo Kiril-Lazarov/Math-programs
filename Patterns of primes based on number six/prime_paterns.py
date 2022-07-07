6347840a18b8716159d36628d2fb4b5eed5624df
Kiril Lazarov <kirillazarov@abv.bg>
2022-07-07 03:30:50 +0300

    Add new deque and new functionalities.

--- Patterns of primes based on number six/prime_paterns.py
+++ Patterns of primes based on number six/prime_paterns.py
@@ -1,6 +1,13 @@
+from collections import deque
+
+
 def primes():
     primes_list = [2, 3]
-    num = int(input("Choose n>3 "))
+    # num = int(input("Choose n>7 "))
+    num = 100
+    if num <= 7:
+        print('Invalid input')
+        primes()
 
     for i in range(4, num):
         is_prime = True
@@ -17,7 +24,8 @@ def patterns():
     data = primes()
     primes_list = data[0]
     n = data[1]
-    for sixts in range(1, n + 1, 6):
+    combinations = deque([])
+    for sixts in range(7, n + 1, 6):
         if sixts in primes_list:
             left = 'True'
         else:
@@ -26,6 +34,18 @@ def patterns():
             right = 'True'
         else:
             right = 'False'
-        print(f'{right} {sixts - 2} --- {sixts - 1} --- {sixts} {left}')
+        combinations.append(right)
+        combinations.append(left)
+
+
+        print(f'{right} {sixts - 2} --- {sixts - 1} --- {sixts} {left}', end='   ')
+    combinations.popleft()
+    combinations.pop()
+    print(combinations)
+    prime_boolean_tuples = []
+    while combinations:
+
+            prime_boolean_tuples.append((combinations.popleft(), combinations.popleft()))
+    print(prime_boolean_tuples)
 
 patterns()
