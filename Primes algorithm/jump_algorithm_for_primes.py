550423763487ae539d06fce531ed1975e4bfc7c2
Kiril Lazarov <kirillazarov@abv.bg>
2022-10-08 21:02:14 +0300

    Algorithm for deriving prime numbers.

---
+++ Primes algorithm/jump_algorithm_for_primes.py
@@ -0,0 +1,38 @@
+import time
+
+end_num = 1999993
+
+n = 2
+last_index = 2
+
+cumulative_list = ['*']
+start = time.time()
+while n <= end_num:
+    for position in cumulative_list:
+        if position == '*':
+            cumulative_list += (2 * n - last_index) * ['*']
+            last_index = 2 * n
+            cumulative_list[-1] = [n]
+            del cumulative_list[0]
+            print(n)
+            # print(cumulative_list)
+            break
+        else:
+            for num in position:
+                if num <= len(cumulative_list) -1:
+                    if cumulative_list[num] == '*':
+                        cumulative_list[num] = [num]
+                    else:
+                        cumulative_list[num].append(num)
+                else:
+                    diff = num - (len(cumulative_list) -1)
+                    last_index += diff
+                    cumulative_list += diff * ['*']
+                    cumulative_list[-1] = [num]
+            del cumulative_list[0]
+            # print(n)
+            # print(cumulative_list)
+            break
+    n += 1
+end = time.time()
+print(f'Time: {end - start} sec')
