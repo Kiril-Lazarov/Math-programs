5e563bdbf890a35f4df808f36959b11822cdb7dc
Kiril Lazarov <kirillazarov@abv.bg>
2022-12-24 16:24:35 +0200

    Refactor the code.

--- length_of_function/length_of_function_line.py
+++ length_of_function/length_of_function_line.py
@@ -1,19 +1,42 @@
-import math
+from math import *
+'''
+    Description:
+    
+    This program calculates the length of a function in a given interval by breaking the curve into small straight lines
+and then summing them up. The accuracy of the result increases with the decrease in the size of the straight lines. 
+'''
 
-n = 10  # bigger n calculates more accurate the length of function
-lower_lim = 0
-upper_lim = 4
-diff = abs(lower_lim - upper_lim)  # defines range
-x_distance = diff / n
-result = 0
-area = 0
-for i in range(1, n + 1):
-    x1 = lower_lim + (i - 1) * x_distance
-    x2 = x1 + x_distance
-    y1 = math.sin(x1)
-    y2 = math.sin(x2)
-    area += abs(x1 - x2) * y1 + abs(x1 - x2) * abs(y1 - y2) / 2  # calculates tha area of the quadrilateral between four points
-    length = math.dist([x1, y1], [x2, y2])
-    result += length
-print(result)
-print(area)
+class FunctionLength:
+    def __init__(self, func: str, down_limit, up_limit, number_of_divisions):
+        self.func = func
+        self.down_limit = down_limit
+        self.up_limit = up_limit
+        self.number_of_divisions = number_of_divisions
+
+    def _replace_x_with_values(self, value):
+        func = self.func.replace('x', str(value))
+        return eval(func)
+
+    def _calculate_y_values(self):
+        length = 0
+        x_interval = abs(self.down_limit - self.up_limit)
+        delta_x = x_interval / self.number_of_divisions
+        x_value = self.down_limit
+        y_value = self._replace_x_with_values(x_value)
+        previous_point = (x_value, y_value)
+        for index in range(1, self.number_of_divisions + 1):
+            x_value = self.down_limit + index * delta_x
+            y_value = self._replace_x_with_values(x_value)
+            next_point = (x_value, y_value)
+            length += dist(previous_point, next_point)
+            previous_point = next_point
+
+        return length
+
+    def __str__(self):
+        return f'The length of "{self.func}" in range {self.down_limit}' \
+               f' and {self.up_limit} is {self._calculate_y_values()}'
+
+
+a = FunctionLength('sin(x)', 0, 2*pi, 1000)
+print(a)
