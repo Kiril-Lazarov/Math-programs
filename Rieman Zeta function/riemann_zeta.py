c44073bb7b90825ac4b6a038699fec6e96cbc9c2
Kiril Lazarov <kirillazarov@abv.bg>
2022-12-30 04:46:53 +0200

    Deleted comments.

--- Rieman Zeta function/riemann_zeta.py
+++ Rieman Zeta function/riemann_zeta.py
@@ -1,18 +1,20 @@
 from matplotlib import pyplot as plt
 
-n = 2000
+
+n = 100
 
 r = []
 i = []
 result = 0 + 0j
 for number in range(1, n+1):
-    result += 1/(number**(1/2 + 4j))
+    result += 1/(number**(1/2 + 21.022039639j))
     r.append(result.real)
     i.append(result.imag)
 
-plt.title("Reimann Zeta partial sum S = 1/2 + 4i n = 2000")
+plt.title("Reimann Zeta partial sum S = 1/2 + 21.022039639i n = 100")
 plt.xlabel('Real')
 plt.ylabel('Imaginary')
 plt.grid(True, color = "k")
 plt.plot(r,i)
 plt.show()
+
