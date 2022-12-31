28996ee6773be0589ec06529290035a4de522bf3
Kiril Lazarov <kirillazarov@abv.bg>
2022-12-31 03:23:08 +0200

    Add description.

--- math/time_dilation.py
+++ math/time_dilation.py
@@ -1,31 +1,50 @@
 from math import sqrt as sq
 
+'''
+    Description:
+    
+    This program tests the hypothesis whether the time flow of a moving body goes faster for the observer when 
+the body approaches it and goes slower when it moves away.  According to the Special theory of relativity, 
+time moves slower in the moving system. This hypothesis agrees with this, but says that there must be a difference 
+in how the passage of time in the moving system looks to the stationary observer. The thought experiment consists in 
+the following - the observer is situated at the origin of the coordinate system (0.0) and the moving body (spacecraft) 
+at a distant point with coordinates x and y (inputs). The ship is moving at some speed close to the speed of light.
+Every second, measured by his own time, he emits a signal from the point at which he is at that moment. The observer 
+receives the signals and measures the time intervals between them. Calculations show that the intervals are shorter 
+when the ship approaches the observer and longer when it moves away from it.
+'''
+
 
 def pythagoras(x, y):
     return sq(x ** 2 + y ** 2)
 
 
+def print_func(message):
+    print(message)
+
+
 def calculate_times_for_observer(sequence_spaceship_points_of_signals, data_dict, time_spaceship):
     y_coordinate = float(f'{data_dict["Distance_y"]:.2f}')
-    velocity = data_dict["Velocity"]
     sequence_observer = []
     path_diff = 0
-    print(f"Spaceship's time run {1 / time_spaceship:.2f} times slower than observer's time.")
+    message = f"Spaceship's time run {1 / time_spaceship:.2f} times slower than observer's time."
+    print_func(message)
     for i in range(len(sequence_spaceship_points_of_signals)):
         x_coordinate = sequence_spaceship_points_of_signals[i]
         # calculates the distance that new signal need to travel after receiving of the previous signal
         if i > 0:
-            path_diff = abs(pythagoras(sequence_spaceship_points_of_signals[i], y_coordinate) - \
+            path_diff = abs(pythagoras(sequence_spaceship_points_of_signals[i], y_coordinate) -
                             (pythagoras(sequence_spaceship_points_of_signals[i - 1],
                                         y_coordinate) - 1 / time_spaceship))
 
-            sequence_observer.append(float(f'{sequence_observer[i-1] + path_diff:.2f}'))
+            sequence_observer.append(float(f'{sequence_observer[i - 1] + path_diff:.2f}'))
         else:
-            sequence_observer.append(float(f'{pythagoras(x_coordinate,y_coordinate):.2f}'))
+            sequence_observer.append(float(f'{pythagoras(x_coordinate, y_coordinate):.2f}'))
 
-        print(f'Signal {i+1}: {sequence_observer[i]:.2f} seconds  '
-              f' x coordinate of current signal {sequence_spaceship_points_of_signals[i]}  Time interval between current'
-              f' and previous signal: {path_diff:.12f}')
+        message = f'Signal {i + 1}: {sequence_observer[i]:.2f} seconds ' \
+                  f' x coordinate of current signal {sequence_spaceship_points_of_signals[i]}   Time interval between current' \
+                  f' and previous signal: {path_diff:.12f}'
+        print_func(message)
 
 
 def coordinates_spaceship_signals_points(data_dict, time_spaceship):
